c21a960456b0c9a6cc4d35ea0a35ea3662b090ab
juanemilionovo-hash <juanemilionovo@gmail.com>
2026-09-07 11:02:07 -0300

    primer_modificacion

--- js/app.js
+++ js/app.js
@@ -82,3 +82,41 @@ if (searchInput && gamesContainer) {
   searchInput.addEventListener("input", filterGames);
   loadGamesData();
 }
+
+
+
+
+/* incio_novedades */
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const gamesGrid = document.querySelector(".games-grid");
+
+    if (gamesGrid) {
+        fetch("./data/games.json")
+            .then(response => response.json())
+            .then(games => {
+                gamesGrid.innerHTML = "";
+            
+                const ids_novedades = [1, 3, 5, 12];
+                const novedades = games.filter(game => ids_novedades.includes(game.id));
+                novedades.forEach(game => {
+                    const article = document.createElement("article");
+                    article.classList.add("game-card");
+
+                    article.innerHTML = `
+                        <img src="${game.image}" alt="${game.title}" class="game-cover">
+                        <div class="card-body">
+                            <h3 class="card-title">${game.title}</h3>
+                            <span class="card-genre">${game.genre}</span>
+                            <div class="card-footer">
+                            </div>
+                        </div>
+                    `;
+
+                    gamesGrid.appendChild(article);
+                });
+            })
+            .catch(error => console.error("Error al cargar los juegos:", error));
+    }
+});
